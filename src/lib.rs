a226c6d5fe8c7afee877ff25d207a612b5ea2c98
Avery Murray <averythumper@gmail.com>
2026-01-24 05:18:54 -0700

    feature: fngr: blocking feature + blocking module

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ pub mod networking;
 pub mod prelude;
 pub mod userlist;
 
-use self::prelude::*;
 use self::networking::{Request, Response};
-
+use self::prelude::*;
 
 pub trait Fngr {
     type SelfLock;
@@ -17,4 +16,21 @@ pub trait Fngr {
     fn list(state: Self::SelfLock, req: Request) -> impl Future<Output = Result<Response>>;
     fn register(state: Self::SelfLock, req: Request) -> impl Future<Output = Result<Response>>;
     fn deregister(state: Self::SelfLock, req: Request) -> impl Future<Output = Result<Response>>;
+}
+
+#[cfg(feature = "blocking")]
+mod blocking {
+    use super::prelude::*;
+    use crate::networking::{Request, Response};
+
+    pub trait Fngr {
+        fn login(&mut self, req: Request) -> Result<Response>;
+        fn logoff(&mut self, req: Request) -> Result<Response>;
+        fn finger(&mut self, req: Request) -> Result<Response>;
+        fn check(&mut self, req: Request) -> Result<Response>;
+        fn bump(&mut self, req: Request) -> Result<Response>;
+        fn list(&mut self, req: Request) -> Result<Response>;
+        fn register(&mut self, req: Request) -> Result<Response>;
+        fn deregister(&mut self, req: Request) -> Result<Response>;
+    }
 }
