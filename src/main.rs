9c14519ba9b141bcb5f8a42686aaad8c591bda23
Avery Murray <averythumper@gmail.com>
2026-03-03 13:07:20 -0700

    feature: set website

--- src/main.rs
+++ src/main.rs
@@ -536,7 +536,35 @@ async fn website(
     addr: Option<&str>,
     hauth: Option<Authorization>,
 ) -> Result {
-    unimplemented!()
+    let auth = match auth_filter(auth, hauth) {
+        Ok(a) => a,
+        Err(e) => {
+            error!("{}", e);
+            return (http::Status::Unauthorized, jobject::Error(e.to_string()).to_string())
+        }
+    };
+
+    let username: Username = match username.parse() {
+        Ok(u) => u,
+        Err(e) => {
+            error!("{}", e);
+            return (http::Status::new(500), jobject::Error(e.to_string()).to_string())
+        }
+    };
+
+    let mut db = DATABASE.lock().unwrap();
+    if let Err(e) = db.authorize(&username, auth.to_string()) {
+        error!("{}", e);
+        return (http::Status::new(401), jobject::Error(e.to_string()).to_string())
+    }
+
+    match db.set_user_website(&username, addr) {
+        Ok(_) => (http::Status::new(200), "{ \"Ok\": \"Your website is set\" }".to_owned()),
+        Err(e) => {
+            error!("{}", e);
+            (http::Status::new(500), jobject::Error(e.to_string()).to_string())
+        }
+    }
 }
 
 #[post("/<fingerprint>/<snuggled>/<from>")]
