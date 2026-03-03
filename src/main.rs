e32a66916cfb5dcbbde0cddf157b30af7f5d3646
Avery Murray <averythumper@gmail.com>
2026-03-03 13:03:41 -0700

    feature: set bio

--- src/main.rs
+++ src/main.rs
@@ -421,11 +421,35 @@ async fn set_bio(
     bio: Option<&str>,
     hauth: Option<Authorization>,
 ) -> Result {
-    // let mut db = DATABASE.lock().unwrap();
-    // let username = username.parse().unwrap();
-    // db.set_user_bio(&username, bio).unwrap();
-    // (http::Status::new(200), "{ \"Ok\": \"status set\" }".to_owned())
-    unimplemented!()
+    let auth = match auth_filter(auth, hauth) {
+        Ok(a) => a,
+        Err(e) => {
+            error!("{}", e);
+            return (http::Status::Unauthorized, jobject::Error(e.to_string()).to_string())
+        }
+    };
+
+    let username: Username = match username.parse() {
+        Ok(u) => u,
+        Err(e) => {
+            error!("{}", e);
+            return (http::Status::new(500), jobject::Error(e.to_string()).to_string())
+        }
+    };
+
+    let mut db = DATABASE.lock().unwrap();
+    if let Err(e) = db.authorize(&username, auth.to_string()) {
+        error!("{}", e);
+        return (http::Status::new(401), jobject::Error(e.to_string()).to_string())
+    }
+
+    match db.set_user_bio(&username, bio) {
+        Ok(_) => (http::Status::new(200), "{ \"Ok\": \"Your bio is set\" }".to_owned()),
+        Err(e) => {
+            error!("{}", e);
+            (http::Status::new(500), jobject::Error(e.to_string()).to_string())
+        }
+    }
 }
 
 #[get("/addsocial?<username>&<auth>&<name>&<string>")]
